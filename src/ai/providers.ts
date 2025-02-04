2295d94085ad7b52d7ae916a96ec231734cbd6b2
0xrsydn <rafayrussani@gmail.com>
2025-02-04 17:04:51 +0700

    added openrouter and anthropic model

--- src/ai/providers.ts
+++ src/ai/providers.ts
@@ -1,5 +1,7 @@
 import { createOpenAI } from '@ai-sdk/openai';
+import { createAnthropic } from '@ai-sdk/anthropic';
 import { getEncoding } from 'js-tiktoken';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 
 import { RecursiveCharacterTextSplitter } from './text-splitter';
 
@@ -7,6 +9,14 @@ import { RecursiveCharacterTextSplitter } from './text-splitter';
 
 const openai = createOpenAI({
   apiKey: process.env.OPENAI_KEY!,
+});
+
+const anthropic = createAnthropic({
+  apiKey: process.env.ANTHROPIC_API_KEY!,
+});
+
+const openrouter = createOpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY!,
 });
 
 // Models
@@ -17,10 +27,38 @@ export const gpt4Model = openai('gpt-4o', {
 export const gpt4MiniModel = openai('gpt-4o-mini', {
   structuredOutputs: true,
 });
+// Using openai for o3-mini & o3-preview
 export const o3MiniModel = openai('o3-mini', {
   reasoningEffort: 'medium',
   structuredOutputs: true,
 });
+export const o3Model = openai('o3-preview', {
+  reasoningEffort: 'medium',
+  structuredOutputs: true,
+});
+
+// Using Anthropic's Claude Sonnet 3.5 model via direct API shas a rate limit of 40k input tokens per minute (for tier 1 users),
+// which is too restrictive for our deep research use case
+export const claudeSonnetModel = anthropic('claude-3-5-sonnet-20241022', {
+  structuredOutputs: true,
+});
+
+// Using openrouter, claude sonnet 3.5 one working so far, other models need more adjustments & testing
+export const openrouterSonnetModel = openrouter('anthropic/claude-3.5-sonnet', {
+  structuredOutputs: true,
+});
+// export const openrouterO3MiniModel = openrouter('openai/o3-mini', {
+//   structuredOutputs: true,
+// });
+// export const openrouterO1Model = openrouter('openai/o1', {
+//   structuredOutputs: true,
+// });
+// export const openrouterGpt4Model = openrouter('openai/chatgpt-4o-latest', {
+//   structuredOutputs: true,
+// });
+// export const deepseekV3Model = openrouter('deepseek/deepseek-chat', {
+//   structuredOutputs: true,
+// });
 
 const MinChunkSize = 140;
 const encoder = getEncoding('o200k_base');
@@ -49,7 +87,7 @@ export function trimPrompt(prompt: string, contextSize = 120_000) {
   });
   const trimmedPrompt = splitter.splitText(prompt)[0] ?? '';
 
-  // last catch, there's a chance that the trimmed prompt is same length as the original prompt, due to how tokens are split & innerworkings of the splitter, handle this case by just doing a hard cut
+  // last catch, there's a chance that the trimmed prompt is same length as the original prompt, handle by hard cutting
   if (trimmedPrompt.length === prompt.length) {
     return trimPrompt(prompt.slice(0, chunkSize), contextSize);
   }
